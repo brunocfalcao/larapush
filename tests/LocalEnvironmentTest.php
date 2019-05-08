1ad6414111d46e4495f7757a400b53ea09d4bc8a
brunocfalcao <bruno.c.falcao@brunofalcao.me>
2019-05-08 13:18:00 +0200

    changes on unit testing

--- tests/LocalEnvironmentTest.php
+++ tests/LocalEnvironmentTest.php
@@ -11,7 +11,21 @@ class LocalEnvironmentTest extends TestCase
      */
     public function assertLocalCreateTransaction()
     {
-        Local::createRepository(generate_transaction_code());
+        $transaction = generate_transaction_code();
+        Local::createRepository($transaction);
+
+        // Verify if local repository was created, along with the runbook.json and the codebase.zip file.
+        $transactionRepository = app('config')->get('larapush.storage.path') . '/' . $transaction;
+
+        // Directory exists?
+        if (!is_dir($transactionRepository)) {
+            return $this->fail();
+        }
+
+        // codebase.zip and runbook.json exists?
+        if (!is_file("$transactionRepository/codebase.zip") || !is_file("$transactionRepository/runbook.json")) {
+            return $this->fail();
+        }
 
         $this->assertTrue(true);
     }
